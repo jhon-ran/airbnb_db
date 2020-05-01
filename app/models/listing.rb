b5d2e158c8e97a9b517d5137c1d99126bccc9c9b
jhon-ran <jhonnatan.rangel@gmail.com>
2020-05-01 14:19:07 +0200

    created & debug seeds

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,5 @@
 class Listing < ApplicationRecord
-  belongs_to :city
-  belongs_to :admin, foreign_key: 'admin_id', class_name: 'User'
   has_many :reservations
-  has_many :users, through: :reservations
+  belongs_to :city
+  belongs_to :admin, class_name: "User"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,54 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-City.destroy_all
 User.destroy_all
+Reservation.destroy_all
+Listing.destroy_all
+City.destroy_all
 require 'faker'
 
 # Seed for City
-10.times do
-  City.create!(
+10.times do |index|
+  City.create(
     name: Faker::Address.city,
-    zip_code: Faker::Address.zip.first(5)
-    )
+    zip_code: "33#{index.to_s}00"
+  )
 end
 
 # Seed for User
 20.times do
   User.create!(
     email: Faker::Internet.email,
-    phone_number: "+33666666677"
+    phone_number: "+33666666677",
+    description: Faker::Lorem.paragraph(sentence_count: 25)
   )
+end
+
+# Seed for Listing
+50.times do 
+  Listing.create(
+    available_beds: rand(1..5),
+    price: rand(10..100),
+    description: Faker::Lorem.paragraph(sentence_count: 15),
+    has_wifi: Faker::Boolean.boolean,
+    welcome_message: "Greetings from your next accomodations",
+    city: City.all.sample,
+    admin: User.all.sample
+  )
+end
+
+# Seed for Reservation
+Listing.all.each do |listing|
+	5.times do |index|
+		index = index - 5
+		length = rand(5)
+		start_date = Time.now + ((index)*5).day
+		end_date = start_date + length.day 
+		Reservation.create(
+	    start_date: start_date,
+	    end_date: end_date,
+	    guest: User.all.sample,
+	    listing: listing
+		)
+	end
 end
